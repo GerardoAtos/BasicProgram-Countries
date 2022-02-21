e26a2d4149690ea1ea1faba445ab6951996e7413
Gerardo Arturo Manzano <gerardo-arturo.manzano@atos.net>
2022-02-21 12:44:40 -0600

    mi primer commit

---
+++ ConsoleApp1/ClassMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ClassMethod
+    {
+        public void GreetCountries(string inputFile, string outputFile, string path)
+        {
+            string content = "";
+            string endPath = path + outputFile;
+            try
+            {
+                using(StreamReader oSR = File.OpenText(path + inputFile))
+                {
+                    string country = "";
+                    while((country = oSR.ReadLine()) != null)
+                    {
+                        content += "Saludos hasta "+ country + "!\n";
+                    }
+                    File.AppendAllText(endPath, content);
+                    Console.WriteLine("File generated successfully!!!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp1;
+
+string path = "C:\\Users\\a847065\\Documents\\C#\\";
+string inputFile = "countries.txt";
+string outputFile = "coutries "+ DateTime.Now.ToString("yyyy-MMMM-d")+".txt";
+
+
+ClassMethod obj = new ClassMethod();
+obj.GreetCountries(inputFile, outputFile, path);
